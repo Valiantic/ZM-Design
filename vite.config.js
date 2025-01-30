9cbb24e4f5c246132d1332f89ce63962fa4d9862
StevenMadali <stevenmadali17@gmail.com>
2025-01-30 16:07:37 +0800

    Add Another ViteCSS Configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import viteCSS from 'vite-plugin-css';
+
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteCSS(),
   ],
   build: {
     rollupOptions: {
